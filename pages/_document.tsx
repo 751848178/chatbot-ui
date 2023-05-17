f06ad281f36a5292e8d5337b5d113fefedfedd70
494593337@qq.com <494593337@qq.com>
2023-05-18 00:52:03 +0800

    feat: add data statistics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,11 @@ export default function Document(props: Props) {
       <Head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Chatbot UI"></meta>
+        {/* <script charSet="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
+        <script dangerouslySetInnerHTML={{__html: `LA.init({id:"K4ErbxGT8UXakPaV",ck:"K4ErbxGT8UXakPaV",autoTrack:true,hashMode:true})` }}/> */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          !function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"K4ErbxGT8UXakPaV",ck:"K4ErbxGT8UXakPaV",autoTrack:true,hashMode:true});
+        ` }}/>
       </Head>
       <body>
         <Main />
